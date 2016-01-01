62abbdfd203e3e93c68d7d592d43c18b0fae6862
Ubuntu <terrenjpeterson@yahoo.com>
2016-01-01 00:05:41 +0000

    first version of camera applicaiton

---
+++ camera_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# this program will capture a photo on a camera attached to a raspberry pi
+
+import picamera
+import time
+import datetime
+
+# capture the current date and time in string format
+curr_dt_tm = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+print curr_dt_tm
+
+# create an instance of the camera
+
+camera = picamera.PiCamera()
+
+# set resolution of the camera
+camera.resolution = (2400, 1200)
+camera.start_preview()
+
+# overlay text on top of the photo
+camera.annotate_text = 'Hello My Young Plants!\n' + curr_dt_tm
+
+time.sleep(2)
+
+# capture the photo to a local file system
+camera.capture('test.jpg')
