88719b5ddcbd2819b9634a8f0514940bb61bf387
Ubuntu <terrenjpeterson@yahoo.com>
2016-01-17 22:29:24 +0000

    updated tweet logic to string in current garden info

--- camera/tweet.py
+++ camera/tweet.py
@@ -9,6 +9,11 @@ from twitter import *
 # this handles reading in the twitter.config file with the secret keys
 import ConfigParser
 
+# this handles the API calls back to AWS
+import requests
+
+import json
+
 # read in the configuration file
 configParser = ConfigParser.RawConfigParser()
 configFilePath = r'twitter.config'
@@ -23,8 +28,21 @@ API_secret = configParser.get('My Secret Data', 'API_secret')
 # create OAuth token using the secret keys  
 twitter = Twitter(auth = OAuth(access_token, access_token_secret, API_key, API_secret))
 
-# create what information is to be tweeted
-new_status = "the lights are doing all of the work"
+# get the current garden conditions
+garden = requests.get("https://xzmez6qe1a.execute-api.us-west-2.amazonaws.com/prod")
 
-# post tweet
-results = twitter.statuses.update(status = new_status)
+if garden.status_code == 200:
+  garden_data = json.loads(garden.content)
+
+  # parse through the JSON data returned from the http request for current data
+  humidity = garden_data['humidity']
+  temperature = garden_data['temperature']
+  read_date = garden_data['read_date']
+  read_time = garden_data['read_time']
+
+  # this is the new string that will be posted to twitter
+  new_status = "Currently in the garden, the temperature is %s F and relative humidity is %s at %s." % (temperature, humidity, read_time[:5])
+  # new_status = "the lights are doing all of the work"
+
+  # post tweet
+  new_update = twitter.statuses.update(status = new_status)
