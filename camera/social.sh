5a1f29d51d319e00c9118834ea32f7e898995bf1
Ubuntu <terrenjpeterson@yahoo.com>
2016-02-22 13:03:56 +0000

    added time parameters to not take pictures in the dark

--- camera/social.sh
+++ camera/social.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# run script that will tweet and upload photos
+# this script is used for driving the daily processing of the pi
+# within the loop the function will check the current time
+# and if the time is within the parameters, will upload a photo
 
 for i in {1..1000}
 do
-    sudo python tweet.py
-    sleep 10s
-    sudo python upload_img.py
-    sleep 2h
-    sudo python upload_img.py
-    sleep 2h
-    sudo python upload_img.py
-    sleep 2h
+    time=`date +%k%M` ;
+    echo $time
+    if [ "$time" -le 700 ]
+      then 
+      echo 'early'
+    elif [ "$time" -ge 1800 ]
+      then
+      echo 'late'
+    else
+      sudo python upload_img.py
+    fi
+    sleep 1h
+    # sudo python tweet.py
 done
